b248ce56de4545ddd2d072eec552cb28c9474878
Your Name <Kedhareswer.12110626@gmail.com>
2025-06-11 22:18:15 +0530

    again

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com", "github.com"],
     unoptimized: true,
   },
   experimental: {
-    serverActions: true,
+    serverActions: {
+      allowedOrigins: ["localhost:3000"],
+    },
   },
   env: {
     // No sensitive API keys here
